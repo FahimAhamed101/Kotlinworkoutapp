e7e7c3140a226ef424b4e0884a336c090edeef39
fahim <fahimahamedweb@gmail.com>
2024-03-08 01:59:42 +0600

    exercise section done

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -1,11 +1,158 @@
 package com.example.a7minutesworkout
 
+import android.app.Dialog
+import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.a7minutesworkout.databinding.ActivityExerciseBinding
+import java.util.*
+import kotlin.collections.ArrayList
+import com.example.a7minutesworkout.databinding.DialogCustomBackConfirmationBinding
 class ExerciseActivity : AppCompatActivity() {
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
+
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
+    //sdasdadadsadadasdadsadadadasdadadadasdasdadadadadadasdasdasdasdadasdasdasdasd
+    private var exerciseTimerDuration: Long = 1
+
+    private var exerciseList: ArrayList<ExerciseModel>? = null
+    private var currentExercisePosition = -1
+
+    private var player: MediaPlayer? = null
+
+    private var binding: ActivityExerciseBinding? = null
+
+    private var exerciseAdapter: ExerciseStatusAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_exercise)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        exerciseList = Constants.defaultExerciseList()
+
+        setupRestView()
+        setUpExerciseStatusRecycleView()
+    }
+
+    private fun setUpExerciseStatusRecycleView(){
+        binding?.rvExerciseStatus?.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        exerciseAdapter = ExerciseStatusAdapter(exerciseList!!)
+        binding?.rvExerciseStatus?.adapter = exerciseAdapter
+    }
+    private fun setupRestView() {
+
+        try{
+            val soundURI = Uri.parse("android.resource://com.example.a7minutesworkoutapp/${R.raw.press_start}")
+            player = MediaPlayer.create(this, soundURI)
+            player?.isLooping = false
+            player?.start()
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+
+
+        binding?.tvNameOfUpcomingExercise?.text = exerciseList!![currentExercisePosition + 1].getName()
+
+        binding?.flRestView?.visibility = View.VISIBLE
+        binding?.tvTitle?.visibility = View.VISIBLE
+        binding?.tvUpcomingExercise?.visibility = View.VISIBLE
+        binding?.tvNameOfUpcomingExercise?.visibility = View.VISIBLE
+        binding?.tvExerciseName?.visibility = View.INVISIBLE
+        binding?.flExerciseView?.visibility = View.INVISIBLE
+        binding?.ivImage?.visibility = View.INVISIBLE
+
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding?.progressBar?.progress = restProgress
+
+        restTimer = object : CountDownTimer(1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                currentExercisePosition++
+
+                exerciseList!![currentExercisePosition].setIsSelected(true)
+                exerciseAdapter!!.notifyDataSetChanged()
+
+                setupExerciseView()
+            }
+        }.start()
+    }
+
+    private fun setupExerciseView() {
+        binding?.flRestView?.visibility = View.INVISIBLE
+        binding?.tvTitle?.visibility = View.INVISIBLE
+        binding?.tvExerciseName?.visibility = View.VISIBLE
+        binding?.flExerciseView?.visibility = View.VISIBLE
+        binding?.ivImage?.visibility = View.VISIBLE
+        binding?.tvUpcomingExercise?.visibility = View.INVISIBLE
+        binding?.tvNameOfUpcomingExercise?.visibility = View.INVISIBLE
+
+        if (exerciseTimer != null) {
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+
+        binding?.ivImage?.setImageResource(exerciseList!![currentExercisePosition].getImage())
+        binding?.tvExerciseName?.text = exerciseList!![currentExercisePosition].getName()
+
+        setExerciseProgressBar()
+    }
+
+    private fun setExerciseProgressBar() {
+        binding?.progressBarExercise?.progress = exerciseProgress
+
+        exerciseTimer = object : CountDownTimer(exerciseTimerDuration * 1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                binding?.progressBarExercise?.progress = exerciseTimerDuration.toInt() - exerciseProgress
+                binding?.tvTimerExercise?.text = (exerciseTimerDuration.toInt() - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+
+                if (currentExercisePosition < exerciseList?.size!! - 1) {
+                    exerciseList!![currentExercisePosition].setIsSelected(false)
+                    exerciseList!![currentExercisePosition].setIsCompleted(true)
+                    exerciseAdapter!!.notifyDataSetChanged()
+                    setupRestView()
+                } else {
+                    finish()
+                    val intent = Intent(this@ExerciseActivity, FinishActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }.start()
+    }
+
+    public override fun onDestroy() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        super.onDestroy()
+        binding = null
     }
 }
